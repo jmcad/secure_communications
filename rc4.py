d8ba1870ca1672ac33754922efe4071b252bb2e6
jmcad <jmcadinez@gmail.com>
2021-10-24 15:10:30 +0200

    Implemened KSA and PRNG algorithms

---
+++ rc4.py
@@ -0,0 +1,42 @@
+class RC4:
+    def __init__(self) -> None:
+        pass
+
+    # Key Scheduling Algorithm
+    def ksa(self, key):
+        S = [None]*256
+        T = [None]*256
+        j = 0
+
+        # Initialization
+        for i in range(256):
+            S[i] = i
+            T[i] = key[i % len(key)]
+        
+        # Initial Permutation of S
+        for i in range(256):
+            j = (j + S[i] + T[i]) % 256
+            
+            # Swap
+            S[i], S[j] = S[j], S[i]
+
+            return S
+
+    # Pseudo Random Number Generator Algorithm
+    def prng(self, S):
+        i, j = 0
+        k = []
+
+        while True:
+            i = (i + 1) % 256
+            j = (j + S[i]) % 256
+            
+            S[i], S[j] = S[j], S[i]
+
+            t = (S[i] + S[j]) % 256
+            k.append(S[t])
+    
+    # XOR
+    def xor(self, string, keystream):
+        text = []
+        
