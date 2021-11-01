97fa804cb24e4987c13fcc57aa96192e76b0c7b8
jmcad <jmcadinez@gmail.com>
2021-11-01 01:37:34 +0100

    some changes on initialization part

--- csprng.py
+++ csprng.py
@@ -1,13 +1,15 @@
-def csprng(key, n):
+def rc4(key, n):
     # Key Scheduling Algorithm
-        S = [None]*256
-        T = [None]*256
+        S = list()
+        T = list()  # temporary array
         j = 0
 
         # Initialization
         for i in range(256):
-            S[i] = i
-            T[i] = key[i % len(key)]
+            S.append(i)
+            T.append(key[i % len(key)])
+            # S[i] = i
+            # T[i] = key[i % len(key)]
         
         # Initial Permutation of S
         for i in range(256):
@@ -17,16 +19,18 @@ def csprng(key, n):
             S[i], S[j] = S[j], S[i]
 
     # Pseudo Random Number Generator Algorithm
-        i, j = 0, 0
-        K = []
+        i = 0
+        j = 0
+        K = list()
 
         while n > 0:
             n -= 1
             i = (i + 1) % 256
             j = (j + S[i]) % 256
             
+            # Swap
             S[i], S[j] = S[j], S[i]
 
             t = (S[i] + S[j]) % 256
             K.append(S[t])
-            return K 
+        return K 
